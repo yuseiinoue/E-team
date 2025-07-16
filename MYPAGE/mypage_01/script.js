b03f2754c5571a7c17fa0e697f2ec9c8aaef8280
f24aa008 <kasutera1413@gmail.com>
2025-07-16 09:27:30 +0900

    アコーディオンメニューの追加

--- MYPAGE/mypage_01/script.js
+++ MYPAGE/mypage_01/script.js
@@ -3,3 +3,11 @@ const pointExpiry = "2025/05/25";
 
 document.getElementById("point").textContent = `${userPoint}pt`;
 document.getElementById("expire").textContent = `ポイントの有効期限は${pointExpiry}\nまでです。`;
+
+function toggleMenu(element) {
+    element.classList.toggle("active");
+    const submenu = element.nextElementSibling;
+    if (submenu.classList.contains("submenu")) {
+        submenu.classList.toggle("open");
+    }
+}
